25cd02aa01738a36599e972e312bb6fdbc0e9a3f
smarcotte4g <smarcotte4g@gmail.com>
2018-03-21 14:47:14 -0700

    Added Comic Book Detail View

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -12,11 +12,7 @@ namespace ComicBookGallery.Controllers
         // Action Methods need to be Public
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Wednesday)
-            {
-                return Redirect("/");
-            }
-            return Content("Hello from the comic books controller!");
+            return View();
             
         }
     }
